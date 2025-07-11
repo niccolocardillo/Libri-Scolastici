67a756701e4fb9af9777fda3daa676e7134bf5ca
Niccolo Cardillo <140056960+niccolocardillo@users.noreply.github.com>
2025-07-11 16:12:57 +0200

    Fix

--- scripts/import.sql
+++ scripts/import.sql
@@ -1,17 +1,5 @@
 INSERT INTO LIBRI_RAW 
-SELECT * FROM read_csv('./data/ALTABRUZZO000020250709.csv.gz', header = true, decimal_separator = ',');
-
-INSERT INTO LIBRI_RAW 
-SELECT * FROM read_csv('./data/ALTBASILICATA000020250709.csv.gz', header = true, decimal_separator = ',');
-
-INSERT INTO LIBRI_RAW 
-SELECT * FROM read_csv('./data/ALTCALABRIA000020250709.csv.gz', header = true, decimal_separator = ',');
-
-INSERT INTO LIBRI_RAW 
-SELECT * FROM read_csv('./data/ALTCAMPANIA000020250709.csv.gz', header = true, decimal_separator = ',');
-
-INSERT INTO LIBRI_RAW 
-SELECT * FROM read_csv('./data/ALTTOSCANA000020250709.csv.gz', header = true, decimal_separator = ',');
+SELECT * FROM read_csv('./data/ALT*.csv.gz', header = true, decimal_separator = ',');
 
 INSERT INTO SCUOLE_RAW 
 SELECT * FROM read_csv('./data/SCUANAGRAFESTAT20252620250901.csv.gz', header = true, decimal_separator = ',');
