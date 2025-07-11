5df8c34bf7f683c7c04386e45036833ae562222e
Niccolo Cardillo <140056960+niccolocardillo@users.noreply.github.com>
2025-07-11 15:55:01 +0200

    Update import.sql

--- scripts/import.sql
+++ scripts/import.sql
@@ -1,2 +1,17 @@
-INSERT INTO LIBRI 
-SELECT * FROM read_csv('/data/ALTTOSCANA000020250709.csv', header = true, decimal_separator = ',')
+INSERT INTO LIBRI_RAW 
+SELECT * FROM read_csv('/data/ALTABRUZZO000020250709.csv.gz', header = true, decimal_separator = ',');
+
+INSERT INTO LIBRI_RAW 
+SELECT * FROM read_csv('/data/ALTBASILICATA000020250709.csv.gz', header = true, decimal_separator = ',');
+
+INSERT INTO LIBRI_RAW 
+SELECT * FROM read_csv('/data/ALTCALABRIA000020250709.csv.gz', header = true, decimal_separator = ',');
+
+INSERT INTO LIBRI_RAW 
+SELECT * FROM read_csv('/data/ALTCAMPANIA000020250709.csv.gz', header = true, decimal_separator = ',');
+
+INSERT INTO LIBRI_RAW 
+SELECT * FROM read_csv('/data/ALTTOSCANA000020250709.csv.gz', header = true, decimal_separator = ',');
+
+INSERT INTO SCUOLE_RAW 
+SELECT * FROM read_csv('/data/SCUANAGRAFESTAT20252620250901.csv.gz', header = true, decimal_separator = ',');
